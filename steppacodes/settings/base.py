7dce07f5e07ba817537b1ec1990ef0efac9b5514
Big Steppa <steppacodes@gmail.com>
2024-03-21 21:48:11 +0100

    Fix: static files

--- steppacodes/settings/base.py
+++ steppacodes/settings/base.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
