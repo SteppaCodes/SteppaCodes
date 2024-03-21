a8332677d26da49c770494a06b83b940544ce75d
Big Steppa <steppacodes@gmail.com>
2024-03-21 17:25:17 +0100

    Add: app for vercel

--- steppacodes/wsgi.py
+++ steppacodes/wsgi.py
@@ -6,4 +6,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'steppacodes.settings.{config("SETTINGS")}')
 
+
 application = get_wsgi_application()
+app = application
+
